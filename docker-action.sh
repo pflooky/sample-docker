7f8cc7dfc5ec48ad828de73488dc7211132fa737
Flook Peter <pflooky@hotmail.com>
2023-08-07 09:57:28 +0800

    Fix folder path of dockerfile

--- docker-action.sh
+++ docker-action.sh
@@ -11,7 +11,8 @@ dockerfiles=$(find . -name Dockerfile)
 for df in $dockerfiles
 do
   docker_image=$(echo "$df" | awk -F "/" '{print $(NF-2)"-"$(NF-1)}')
-  echo "Building $docker_image"
+  folder_path=$(echo "$df" | sed -E 's/\.\/|Dockerfile//g')
+  echo "Building $docker_image at $folder_path"
   docker buildx build --platform $platforms -t pflookyy/"$docker_image":latest --push "$df"
 done
 
